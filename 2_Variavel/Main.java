17ea06ae5e4ecac95c0949bd315235f9553e1969
PedroLucasPinheiroSoares <pedrolpsoareshvr@gmail.com>
2026-08-19 20:34:48 -0300

    aula2

---
+++ 2_Variavel/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        String nome = "Pedro";
+        String sobrenome = "Pinheiro";
+        int idade = 18;
+        double altura = 1.80;
+        boolean empregado = true;
+        String telefone = "+55 (15) 99681-5248";
+        String endereco = "Rua Francisco Bernardes, 551";
+        String cep = "18.151-500";
+        String uf = "SP";
+        double salario = 2000.00;
+        double valor_hora_extra = 100.00;
+        System.out.println("Nome do aluno: " + nome + " " + sobrenome);
+
+    }
+}
