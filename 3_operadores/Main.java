c35fff388d5a2db7aaa0afb2be3a2dc9366c858b
PedroLucasPinheiroSoares <pedrolpsoareshvr@gmail.com>
2026-08-19 22:19:11 -0300

    manipulacao de string

---
+++ 3_operadores/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args){
+        // int valor_1 = 10;
+        // int valor_2 = 20;
+        // int soma = 0;
+
+        // soma = valor_1 + valor_2;
+
+        // System.out.println("Valor 1 = " + valor_1);
+        // System.out.println("Valor 2 = " + valor_2);
+        // System.out.println("Soma dos valores = " + soma);
+
+
+
+        // -----------------------------------------------------------------
+
+
+
+        // double valor = 5;
+        // System.out.println("O valor " + valor + " possui sobra de: " + valor % 2);
+
+
+
+        // -----------------------------------------------------------------
+
+
+
+        int contador = 0;
+
+        System.out.println(contador++);
+        System.out.println(contador--);
+    }
+    
+}
