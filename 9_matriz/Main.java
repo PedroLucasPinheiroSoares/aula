9592d1c7fc5f5fb73c1b834c1bad795f94232d03
PedroLucasPinheiroSoares <pedrolpsoareshvr@gmail.com>
2026-09-09 20:28:57 -0300

    segundo comit 09-09

--- 9_matriz/Main.java
+++ 9_matriz/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String [] args){
 
         //int[][] numeros= { {1, 4, 2, 0}, {3, 6, 8, 7}, {70, 5968, 5, 514326}, {542, 67, 69, 2020207}};
+
         int[][] numeros= { 
             {1, 4, 2, 0}, 
             {3, 6, 8, 7}, 
@@ -10,6 +11,30 @@ public class Main {
             
         System.out.println(numeros[2][3]); // Saída 514326
 
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.println(numeros[i][j]); // Exibe a matriz
+            }
+        }
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                numeros[i][j] = 0; // Vai zerar todos os valores da matriz percorrendo ela
+            }
+        }
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.println(numeros[i][j]); // Exibe todos os elementos da matriz
+            }
+        }
+
+        numeros[2][3] = 67;
+        System.out.println(numeros[2][3]);
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.println(numeros[i][j]); // Exibe todos os elementos da matriz
+            }
+        }
     }
-    
 }
