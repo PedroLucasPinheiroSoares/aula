d7087d101688d7d70981f703813494832ba95983
PedroLucasPinheiroSoares <pedrolpsoareshvr@gmail.com>
2026-09-09 19:57:15 -0300

    aula de matriz

---
+++ 9_matriz/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String [] args){
+
+        //int[][] numeros= { {1, 4, 2, 0}, {3, 6, 8, 7}, {70, 5968, 5, 514326}, {542, 67, 69, 2020207}};
+        int[][] numeros= { 
+            {1, 4, 2, 0}, 
+            {3, 6, 8, 7}, 
+            {70, 5968, 5, 514326}, 
+            {542, 67, 69, 2020207}};
+            
+        System.out.println(numeros[2][3]); // Saída 514326
+
+    }
+    
+}
