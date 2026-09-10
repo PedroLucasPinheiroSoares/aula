9373a577f50f9a0fc457433f1cf3485e5a797471
PedroLucasPinheiroSoares <pedrolpsoareshvr@gmail.com>
2026-09-09 22:26:51 -0300

    comit final - lembra de termiar os exercicios - 0909

--- exercicios_0909/Main.java
+++ exercicios_0909/Main.java
@@ -6,7 +6,12 @@ public class Main {
         //                                          Lista de Exercício Prático - Java
 
 
-       //             EXERCICIO 1 
+
+
+
+
+
+       //                             EXERCICIO 1 
 
         // int num_1 = 10, num_2 = 50;
 
@@ -18,20 +23,168 @@ public class Main {
 
 
 
-        //              EXERCICIO 2
 
-        int num_1 = 10, num_2 = 3;
-        if(num_1 % 2 == 0){
-            System.out.println(num_1 + " e par");
-        }else{
-            System.out.println(num_1 + " e impar");
-        }
 
-        if(num_2 % 2 == 0){
-            System.out.println(num_2 + " e par");
-        }else{
-            System.out.println(num_2 + " e impar");
-        }
+
+
+
+
+
+
+
+
+        //                          EXERCICIO 2
+
+        // int num_1 = 10, num_2 = 3;
+        // if(num_1 % 2 == 0){
+        //     System.out.println(num_1 + " e par");
+        // }else{
+        //     System.out.println(num_1 + " e impar");
+        // }
+
+        // if(num_2 % 2 == 0){
+        //     System.out.println(num_2 + " e par");
+        // }else{
+        //     System.out.println(num_2 + " e impar");
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //                          EXERCICIO 3
+
+        // double nota = 3.9;
+
+        // if(nota > 6 || nota == 6){
+        //     System.out.println("Nota " + nota + "!  Aprovado uhul");
+        // }else if(nota < 4){
+        //     System.out.println("Nota " + nota + "!   Reprovadooo");
+        // }else{
+        //     System.out.println("Nota " + nota + "! Recuperacao, boa sorte");
+        // }
+
+
+
+
+
+
+
+
+
+
+                //              EXERCICIO 4
+
+        // int num_1 = 10, num_2 = 5, num_3 = 3;
+
+        // if(num_1 > num_2 && num_1 > num_3){
+        //     System.out.println("Numero " + num_1 + " e maior que " + num_2 + " e " + num_3);
+        // }else if(num_2 > num_1 && num_2 > num_3){
+        //     System.out.println("Numero " + num_2 + " e maior que " + num_1 + " e " + num_3);
+        // }else{
+        //     System.out.println("Numero " + num_3 + " e maior que " + num_1 + " e " + num_2);
+        // }
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+        //                     EXERCICIO 5
+
+        // int i = 1;
+
+        // while (i < 1001) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //                      EXERCICIO 6
+
+        // for(int i = 1; i < 101; i++){
+        //     if(i % 2 == 0){
+        //         System.out.println(i);
+        //     }else{
+        //         System.out.print(" ");
+        //     }
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        //                      EXERCICIO 7
+
+        // for(int i = 10; i > 0; i--){
+        //     System.out.println(i);
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //                      EXERCICIO 8
+
+
+        // for(int num = 1; num < 11; num++){
+        //     for(int i = 1; i < 11; i ++){
+        //         System.out.println(num + " x " + i + " = " + (num * i));
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+
 
 
     }
